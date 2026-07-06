1e472fb970b94e7de714a500a53293707dd9a134
Ashfaque <ashfaquemuhammed94@gmail.com>
2026-07-06 20:40:40 +0530

    cors policy added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]'
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,8 +23,8 @@ export default defineConfig({
       },
       '/socket.io': {
         target: 'https://interachat-backend.onrender.com',
-        ws: true,
-        changeOrigin: true
+        changeOrigin: true,
+        ws: true
       },
       '/uploads': {
         target: 'https://interachat-backend.onrender.com',
